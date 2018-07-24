d400983cb03ef7dfe5ba92fc84b47788c6a00efb
yangjiayi <415878598@qq.com>
2018-07-24 14:33:34 +0800

    构建二叉树

---
+++ offer-4.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回构造的TreeNode根节点
+    def reConstructBinaryTree(self, pre, tin):
+        last = None
+        if len(pre) == 0:
+            return None
+        elif len(pre) == 1:
+            return TreeNode(pre[0])
+        else:
+            root = TreeNode(pre[0])
+            pos = tin.index(pre[0])
+            root.left = self.reConstructBinaryTree(pre[1:pos+1], tin[:pos])
+            root.right = self.reConstructBinaryTree(pre[pos+1:], tin[pos+1:])
+        return root
+        
