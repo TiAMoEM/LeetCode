c8c73ab8956ccdf903973e0eed24a58e66bcfc01
yangjiayi <415878598@qq.com>
2018-08-09 15:35:28 +0800

    LeetCode第66题

---
+++ 66-plusOne.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        a = 0
+        for i in digits:
+            a = a * 10 + i
+        b = int(a) + 1
+        ans = []
+        for i in str(b):
+            ans.append(int(i))
+        return ans
+
