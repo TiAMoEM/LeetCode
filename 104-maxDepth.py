387f0031c88259114c3a1a679ee6d503792f271b
yangjiayi <415878598@qq.com>
2019-02-25 18:16:43 +0800

    LeetCode第104题python

---
+++ 104-maxDepth.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode):
+        """
+        :type root: Tree
+        """
+        stack = []
+        if root != None:
+            stack.append((1, root))
+        depth = 0
+        while stack != []:
+            currDepth, root = stack.pop()
+            if root != None:
+                depth = max(currDepth, depth)
+                stack.append((currDepth + 1, root.left))
+                stack.append((currDepth + 1, root.right))
+        return depth
