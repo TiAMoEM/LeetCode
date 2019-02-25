224673470b5b895a7d184b5c7a5e0250c46990b1
yangjiayi <415878598@qq.com>
2019-02-25 18:17:08 +0800

    LeetCode第101题java

---
+++ 101-SymmetricTree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Sym(root.left, root.right);
+    }
+
+    public static boolean Sym(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return (Sym(left.left, right.right) && Sym(left.right, right.left));
+    }
+
+}
