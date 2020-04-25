55ac0c81c86f51b86fc15ece26e57819d9c7e426
yangjiayi <yangjiayi@kanzhun.com>
2020-04-25 16:40:47 +0800

    动态规划方法来解抢劫问题

--- 198-rob.py
+++ 198-rob.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def rob(self, nums):
         """
+        动态规划，dp[0] = 0, dp[1] = nums[1]
+        dp[n] = max(dp[n-1], dp[n-2] + nums[n])
         :type nums: List[int]
         :rtype: int
         """
@@ -8,9 +10,9 @@ class Solution(object):
             return 0
         if len(nums) <= 2:
             return max(nums)
-        dp = [0 for i in range(0, 2)]
+        dp = [0] * len(nums)
         dp[0] = nums[0]
         dp[1] = max(nums[1], nums[0])
         for i in range(2, len(nums)):
-            dp[i % 2] = max(dp[(i - 1) % 2], dp[(i - 2) % 2] + nums[i])
-        return dp[(len(nums) - 1) % 2]
+            dp[i] = max(dp[(i - 1)], dp[(i - 2)] + nums[i])
+        return dp[(len(nums) - 1)]
