ca4905facdc7b70e05089f460e3150d9a2a8684e
yangjiayi <yangjiayi@kanzhun.com>
2020-04-27 11:46:22 +0800

    选择排序

---
+++ SelectSort.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+def selectSort(arr):
+    for i in range(len(arr) - 1):
+        minIndex = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[minIndex]:
+                minIndex = j
+        if i != minIndex:
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+    return arr
