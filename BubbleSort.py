c077a232fc0105e47037e281bb35ba1606c82b1c
yangjiayi <yangjiayi@kanzhun.com>
2020-04-28 18:02:09 +0800

    冒泡排序

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 def bubbleSort(arr):
     for i in range(len(arr)):
-        for j in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
