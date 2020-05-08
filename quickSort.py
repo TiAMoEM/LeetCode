f5c9389fdf530732a07c48663e153f33feb4f42d
yangjiayi <yangjiayi@kanzhun.com>
2020-05-08 23:29:54 +0800

    快速排序

---
+++ quickSort.py
@@ -0,0 +1,22 @@
+def partition(arr, left, right):
+    pivotkey = arr[left]
+    while left < right:
+        while left < right and arr[right] >= pivotkey:
+            right -= 1
+        arr[left] = arr[right]
+        while left < right and arr[left] <= pivotkey:
+            left += 1
+        arr[right] = arr[left]
+
+    arr[left] = pivotkey
+    return left
+
+def q_sort(arr, left, right):
+    if left < right:
+        pivot = partition(arr, left, right)
+        q_sort(arr, left, pivot-1)
+        q_sort(arr, pivot+1, right)
+    return arr
+
+def quick_sort(arr):
+    return q_sort(arr, 0, len(arr) - 1)
