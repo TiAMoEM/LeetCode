c929faebe9cd4b2a3b1005b91398cac1ccd05a2d
yangjiayi <yangjiayi@kanzhun.com>
2021-03-10 21:54:58 +0800

    LeetCode第181题增加新的解法

--- 181-Employees.sql
+++ 181-Employees.sql
@@ -1,2 +1,8 @@
 select Name as Employee from Employee e1
 where e1.Salary > (select e2.Salary from Employee e2 where e1.ManagerId = e2.Id);
+
+select a.Name as Employee
+from Employee as a
+inner join Employee as b
+on a.ManagerId = b.Id
+and a.Salary > b.Salary
