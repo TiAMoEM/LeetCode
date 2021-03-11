a7566a3d151ba4852cba05283414206099a470f0
yangjiayi <yangjiayi@kanzhun.com>
2021-03-11 21:36:23 +0800

    LeetCode第82题

---
+++ 82-deleteDuplicates.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        temp = ListNode(0)
+        temp.next = head
+        pre, cur = temp, head
+        while cur:
+            while cur.next and cur.val == cur.next.val:
+                cur = cur.next
+            if pre.next == cur:
+                pre = pre.next
+            else:
+                pre.next = cur.next
+            cur = cur.next
+        return temp.next
