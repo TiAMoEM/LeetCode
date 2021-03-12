c8284975aa9d128cbc261dbb6632cff21be8404e
yangjiayi <yangjiayi@kanzhun.com>
2021-03-12 10:12:24 +0800

    LeetCode第62题

---
+++ 62-uniquePaths.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * m for _ in range(n)]
+        dp[0][0] = 1
+        for i in range(1, m):
+            dp[0][i] = dp[0][i - 1]
+        for j in range(1, n):
+            dp[j][0] = dp[j - 1][0]
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[n - 1][m - 1]
