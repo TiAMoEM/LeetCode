38f6fead839858cd35a51fe505a5d5b94090e096
yangjiayi <yangjiayi@kanzhun.com>
2021-03-17 18:11:04 +0800

    LeetCode第283题增加解法

--- 283-moveZeros.py
+++ 283-moveZeros.py
@@ -11,3 +11,22 @@ class Solution:
                 k += 1
         nums[k::] = [0] * (len(nums) - k)
         # return nums
+
+    def moveZeroes2(self, nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        双指针法
+        """
+        i = 0
+        j = 1
+        while j < len(nums):
+            if nums[i] == 0:
+                if nums[j] == 0:
+                    j += 1
+                else:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i += 1
+                    j += 1
+            else:
+                i += 1
+                j += 1
