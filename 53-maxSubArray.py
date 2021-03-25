59b9a0c85e36b82f6e0edeceb1858bdd0de07517
yangjiayi <yangjiayi@kanzhun.com>
2021-03-25 21:26:56 +0800

    LeetCode第53题 - 修改变量名称，以及增加变化求解问题的解法

--- 53-maxSubArray.py
+++ 53-maxSubArray.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def maxSubArray(self, nums):
         """
@@ -7,13 +10,32 @@ class Solution:
         if len(nums) == 0:
             return 0
         else:
-            sum = max = nums[0]
+            sum1 = max1 = nums[0]
             for i in range(1, len(nums)):
-                if sum >= 0:
-                    sum += nums[i]
+                if sum1 >= 0:
+                    sum1 += nums[i]
                 else:
-                    sum = nums[i]
-                if max < sum:
-                    max = sum
-            return max
+                    sum1 = nums[i]
+                if max1 < sum1:
+                    max1 = sum1
+            return max1
 
+    def maxSubArray2(self, nums: List) -> List:
+        """
+        作业帮面试题：
+        返回的是最大的子序列，不只是需要求得最大值
+        :param nums:
+        :return:
+        """
+        result = tmp = nums[0]
+        start, end = 0, 0
+        for i in range(1, len(nums)):
+            if tmp > 0:
+                tmp += nums[i]
+            else:
+                tmp = nums[i]
+                start = i
+            if result < tmp:
+                result = tmp
+                end = i
+        return nums[start: end + 1]
