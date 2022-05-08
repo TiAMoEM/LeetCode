6f3d20e6aa23de3de53ceb1da2ebd59ceb305f90
YJY <yangjiayibill1995@outlook.com>
2022-05-08 15:31:11 +0800

    LeetCode第239题

---
+++ 239-maxSlidingWindow.py
@@ -0,0 +1,17 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        queue = collections.deque()
+        res = []
+        for i, num in enumerate(nums):
+            if queue and queue[0] == i - k:
+                queue.popleft()
+            while queue and queue[-1] < num:
+                queue.pop()
+            queue.append(i)
+            if i >= k - 1:
+                res.append(nums[queue[0]])
+        return res
