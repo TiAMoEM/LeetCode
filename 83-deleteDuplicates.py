f50222d51b847a0dab80b843622588a094b7679f
YJY <yangjiayibill1995@outlook.com>
2022-05-15 16:11:42 +0800

    LeetCode第83题-双指针理解优化

--- 83-deleteDuplicates.py
+++ 83-deleteDuplicates.py
@@ -1,24 +1,23 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
-    def deleteDuplicates(self, head):
+    def deleteDuplicates(self, head: ListNode):
         """
         :type head: ListNode
         :rtype: ListNode
         """
         if head == None or head.next == None:
             return head
-        p = head
-        q = head.next
-        while p != None and q != None:
-            if p.val != q.val:
-                p = q
-                q = q.next
-            else:
-                p.next = q.next
-                q = q.next
+        slow = head
+        fast = head
+        while fast:
+            if fast.val != slow.val:
+                slow.next = fast
+                slow = slow.next
+            fast = fast.next
+        slow.next = None
         return head
