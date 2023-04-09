d1cbb1578698712d459d2415cb19a50e2bea5141
YJY <yangjiayibill1995@outlook.com>
2023-04-09 16:04:34 +0800

    LeetCode第145题树的后序遍历

---
+++ 145-postorderTraversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from typing import Optional, List
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if root is None:
+            return []
+        else:
+            res += self.postorderTraversal(root.left)
+            res += self.postorderTraversal(root.right)
+            res.append(root.val)
+        return res
