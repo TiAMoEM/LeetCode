a916423eca1256c63e24139b58e5fd3e28da8433
YJY <yangjiayibill1995@outlook.com>
2023-04-17 23:32:21 +0800

    code format

--- InsertSort.py
+++ InsertSort.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
-def insertionSort(arr):
+def insertSort(arr):
     for i in range(len(arr)):
-        preIndex = i-1
+        preIndex = i - 1
         current = arr[i]
         while preIndex >= 0 and arr[preIndex] > current:
-            arr[preIndex+1] = arr[preIndex]
+            arr[preIndex + 1] = arr[preIndex]
             preIndex -= 1
-        arr[preIndex+1] = current
+        arr[preIndex + 1] = current
     return arr
